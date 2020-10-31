aa9efd181c1dec50cf1b42e4f4ef4ba7ca737b39
ritwik jha <jha.ritwik.17.9@gmail.com>
2020-10-31 23:50:48 +0530

    Updates

---
+++ precache-manifest.7c96f630d8be6bd8d4e93f4d09463edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d849710c55059d7b28b22a25c9a436b6",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "5df2bc111cc4decf695d",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df2bc111cc4decf695d",
+    "url": "/birthday-reminder/static/js/main.bc7c761a.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
